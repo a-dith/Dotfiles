9e8149687b659de42019d91e3de338bd99c419dc
Adith pp <ppadith93@gmail.com>
2023-07-20 18:16:19 +0530

    updating

---
+++ Dot_file_baskup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cp $HOME/scripts/git.sh $HOME/myrepo/Dotfiles/Dot_file_baskup.sh
+cp $HOME/.config/sxhkd/sxhkdrc $HOME/myrepo/Dotfiles/sxhkdrc
+cp $HOME/.config/sxiv/exec/key-handler $HOME/myrepo/Dotfiles/sxiv_keyboard_shortcuts
+cp $HOME/.bash_aliases $HOME/myrepo/Dotfiles/bash_aliases_arch
+cp $HOME/.bashrc $HOME/myrepo/Dotfiles/bashrc_arch
+xfconf-query -c xfce4-keyboard-shortcuts -lv  |grep custom >$HOME/myrepo/Dotfiles/custom_keyboard_shortcut_arch
