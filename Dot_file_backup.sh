5d1164e525e99dd43f1e4592bf796bc9e0232496
Adith pp <ppadith93@gmail.com>
2023-07-24 02:17:06 +0530

    updated files

--- Dot_file_backup.sh
+++ Dot_file_backup.sh
@@ -1,7 +1,8 @@
+# Bash script to copy important configuratiion files to git
 #!/bin/bash
 set -e
 
-cp $HOME/scripts/git.sh $HOME/myrepo/Dotfiles/Dot_file_backup.sh
+cp $HOME/scripts/git_copy.sh $HOME/myrepo/Dotfiles/Dot_file_backup.sh
 cp $HOME/.config/sxhkd/sxhkdrc $HOME/myrepo/Dotfiles/sxhkdrc
 cp $HOME/.config/sxiv/exec/key-handler $HOME/myrepo/Dotfiles/sxiv_keyboard_shortcuts
 cp $HOME/.bash_aliases $HOME/myrepo/Dotfiles/bash_aliases_arch
